385b9831fdf9fc799a32af5178ec3f09c0925660
Code with Bahri <codewithbahri@gmail.com>
2023-09-20 09:26:11 +0700

    nav 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widget/navigation_push.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,8 +12,95 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: NavigationPush(),
+      home: BottomNavbarWidget(),
     );
   }
 }
 
+class BottomNavbarWidget extends StatefulWidget {
+  const BottomNavbarWidget({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavbarWidget> createState() => _BottomNavbarWidgetState();
+}
+
+class _BottomNavbarWidgetState extends State<BottomNavbarWidget> {
+  final List<Widget> list = const [
+    Text('Home'),
+    Text('Cart'),
+    Text('Favorite'),
+    Text('User'),
+  ];
+
+  int _selectedIndex = 0;
+  List<dynamic> menuItems = [
+    {
+      'icon': Icon(Icons.home),
+      'label': 'Home',
+    },
+    {
+      'icon': Icon(Icons.shopping_bag),
+      'label': 'Cart',
+    },
+    {
+      'icon': Icon(Icons.favorite),
+      'label': 'Favorite',
+    },
+    {
+      'icon': Icon(Icons.person),
+      'label': 'Profile',
+    },
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Bottom Navbar"),
+        actions: const [],
+      ),
+      body: Center(
+        child: list[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        unselectedItemColor: Colors.black87,
+        elevation: 32.0,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: const TextStyle(
+          height: 1.5,
+          fontSize: 12,
+        ),
+        unselectedLabelStyle: const TextStyle(
+          height: 1.5,
+          fontSize: 12,
+        ),
+        items: menuItems.map((i) {
+          return BottomNavigationBarItem(
+            activeIcon: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: const BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.all(Radius.circular(14)),
+              ),
+              child: i['icon'],
+            ),
+            icon: i['icon'],
+            label: i['label'],
+          );
+        }).toList(),
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
